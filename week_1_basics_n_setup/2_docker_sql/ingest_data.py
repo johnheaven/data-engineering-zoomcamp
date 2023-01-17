7e6be735b1418fbbf50001972a8c2c5339b56ed3
John Heaven <john@johnheaven.eu>
2023-01-17 18:34:58 +0100

    added time cols as command-line arguments and simplified import (no need to save CSV)

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -9,7 +9,6 @@ from time import time
 import pandas as pd
 from sqlalchemy import create_engine
 
-
 def main(params):
     user = params.user
     password = params.password
@@ -18,24 +17,17 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-    
-    # the backup files are gzipped, and it's important to keep the correct extension
-    # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
-    else:
-        csv_name = 'output.csv'
-
-    os.system(f"wget {url} -O {csv_name}")
+    datecols = params.datecols.split(',') if params.datecols else None
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df_iter = pd.read_csv(url, iterator=True, chunksize=100000)
 
     df = next(df_iter)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    if datecols:
+        for datecol in datecols:
+            df[datecol] = pd.to_datetime(df[datecol])
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
@@ -49,9 +41,10 @@ def main(params):
             
             df = next(df_iter)
 
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
+            if datecols:
+                for datecol in datecols:
+                    df[datecol] = pd.to_datetime(df[datecol])
+                
             df.to_sql(name=table_name, con=engine, if_exists='append')
 
             t_end = time()
@@ -72,6 +65,7 @@ if __name__ == '__main__':
     parser.add_argument('--db', required=True, help='database name for postgres')
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
     parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--datecols', required=False, help='columns to attempt to convert to date. separate multiple values with comma')
 
     args = parser.parse_args()
 
