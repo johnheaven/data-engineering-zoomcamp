fcb26a93032dcb38205f320faa5a7271936c045d
John Heaven <john@johnheaven.eu>
2023-01-29 19:45:47 +0100

    made various modifications to docker-compose.yaml and the ingest script, so it's now possible to run it all with Docker compose, but still specify environment variables to ingest different data or specify a different table in the PG DB

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -10,32 +10,31 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def main(params):
-    user = params.user
-    password = params.password
-    host = params.host 
-    port = params.port 
-    db = params.db
-    table_name = params.table_name
-    url = params.url
+    username = params.pg_username
+    password = params.pg_password
+    host = params.pg_host 
+    port = params.pg_port 
+    db = params.pg_db
+    table_name = params.pg_table_name
+    url = params.data_url
     datecols = params.datecols.split(',') if params.datecols else None
-
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     df_iter = pd.read_csv(url, iterator=True, chunksize=100000)
 
     df = next(df_iter)
 
+    # cast date columns if they exist
     if datecols:
         for datecol in datecols:
-            df[datecol] = pd.to_datetime(df[datecol])
+            if datecol in df.columns:
+                df[datecol] = pd.to_datetime(df[datecol])
 
+
+    engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{db}')
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-
     while True: 
-
         try:
             t_start = time()
             
@@ -43,7 +42,8 @@ def main(params):
 
             if datecols:
                 for datecol in datecols:
-                    df[datecol] = pd.to_datetime(df[datecol])
+                    if datecol in df.columns:
+                        df[datecol] = pd.to_datetime(df[datecol])
                 
             df.to_sql(name=table_name, con=engine, if_exists='append')
 
@@ -56,17 +56,40 @@ def main(params):
             break
 
 if __name__ == '__main__':
+    import logging
+
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--pg_username', required=False, help='user name for postgres')
+    parser.add_argument('--pg_password', required=False, help='password for postgres')
+    parser.add_argument('--pg_host', required=False, help='host for postgres')
+    parser.add_argument('--pg_port', required=False, help='port for postgres')
+    parser.add_argument('--pg_db', required=False, help='database name for postgres')
+    parser.add_argument('--pg_table_name', required=False, help='name of the table where we will write the results to')
+    parser.add_argument('--data_url', required=False, help='url of the csv file')
     parser.add_argument('--datecols', required=False, help='columns to attempt to convert to date. separate multiple values with comma')
 
-    args = parser.parse_args()
+    cl_args = vars(parser.parse_args())
 
+    # argument tuples in format (name:str, required: bool)
+    db_arg_names = (('pg_username', True), ('pg_password', True), ('pg_host', True), ('pg_port', True), ('pg_db', True), ('pg_table_name', True), ('data_url', True), ('datecols', False))
+    from collections import namedtuple
+    import os
+    Args = namedtuple('args', (arg_name for arg_name, _ in db_arg_names))
+    db_arg_values = []
+    for arg_name, arg_required in db_arg_names:
+        # try to get command-line argument
+        if cl_args.get(arg_name, None):
+            db_arg_values.append(cl_args[arg_name])
+        elif arg_name.upper() in os.environ.keys():
+        # try to get environment variable
+            db_arg_values.append(os.environ[arg_name.upper()])
+        elif not arg_name and arg_required:
+        # if this parameter is required, then raise an error
+            raise ValueError(f'Argument {arg_name} required as command line argument or {arg_name.upper()} as environment variable.')
+    
+    args = Args(*db_arg_values)
     main(args)
+        
+        
+        
